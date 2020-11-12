7e148bb1e041b40ae3b3aae06247da4e4d03031c
andreyz11288 <andreyz11288@gmail.com>
2020-11-12 21:51:22 +0200

    добавил спинер

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const body = document.querySelector('.gallery');
 const formSearch = document.querySelector('#search-form')
 const inputSearch = formSearch.querySelector('input')
 const btn = document.querySelector('.btn')
+const spinner = document.querySelector('#loader')
 
 let clientHeight = document.documentElement.clientHeight
 
@@ -21,11 +22,14 @@ inputSearch.addEventListener('input',_.debounce(inputSearchValue, 1000));
 
 function inputSearchValue() {
 
+  spinner.classList.add('loader')
     console.log(inputSearch.value); 
     body.innerHTML = ''
     apiService.inputValue(inputSearch.value ).then(e => {
         renderCard(e.hits)
-       
+      btn.classList.remove('none')
+    }).finally(() => {
+      spinner.classList.remove('loader')
     });
 }
 
@@ -37,12 +41,15 @@ function renderCard(resul) {
 }
 
 btn.addEventListener('click', () => {
+           
+
+  spinner.classList.add('loader')
   apiService.inputValue(inputSearch.value ).then(e => {
     renderCard(e.hits)
     scrollTo(0, (clientHeight + pageYOffset))
-   
-
-  });
+  }).finally(() => {
+    spinner.classList.remove('loader')
+    });
 })
 
 
