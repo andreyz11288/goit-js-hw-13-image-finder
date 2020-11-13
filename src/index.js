5eb44fa3c3e049eaf2ba8aa3bea636b117182e25
andreyz11288 <andreyz11288@gmail.com>
2020-11-13 22:11:53 +0200

    добавил basiclightbox

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import './js/lazy'
 import inputSearchValue from './js/input'
 
 import btnClick from './js/btn'
-
+import * as basicLightbox from 'basiclightbox'
 import './css/style.css'
+import './css/main.css'
 import refs from './js/refs'
 
 
@@ -25,6 +26,13 @@ refs.btn.addEventListener('click', btnClick)
 // Клик по кнопке
 // btnClick()
 
-
+const form = document.querySelector('.form')
+form.addEventListener('click', (event) => {
+  // event.defaultPrevented();
+  const dataSrc = event.target.dataset.src;
+  const instance = basicLightbox.create(`
+      <img src="${dataSrc}" width="1200" height="800">
+  `).show()
+})
 
 
